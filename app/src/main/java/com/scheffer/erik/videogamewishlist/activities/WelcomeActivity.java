61baf1f3f4c5f63c46142682ccbb7c1174bec359
esscheffer <erikscheffer@yahoo.com>
2018-04-29 19:56:16 -0300

    Create welcome activity to handle cache initialization

---
+++ app/src/main/java/com/scheffer/erik/videogamewishlist/activities/WelcomeActivity.java
@@ -0,0 +1,22 @@
+package com.scheffer.erik.videogamewishlist.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.scheffer.erik.videogamewishlist.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        findViewById(R.id.fullscreen_content)
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                                               | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                               | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                               | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                                               | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                               | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+}
