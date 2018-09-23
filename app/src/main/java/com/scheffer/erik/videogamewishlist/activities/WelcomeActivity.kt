ccc922f123efa3c6efbb01a6d9cd68bce0497c1f
esscheffer <erikscheffer@yahoo.com>
2018-09-23 11:42:45 -0300

    Added consent check

--- app/src/main/java/com/scheffer/erik/videogamewishlist/activities/WelcomeActivity.kt
+++ app/src/main/java/com/scheffer/erik/videogamewishlist/activities/WelcomeActivity.kt
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
+import com.google.ads.consent.*
 import com.scheffer.erik.videogamewishlist.BuildConfig
 import com.scheffer.erik.videogamewishlist.R
 import com.scheffer.erik.videogamewishlist.syncadapter.SyncAdapter.Companion.PREF_GENRES_SYNC
@@ -17,10 +18,12 @@ import com.scheffer.erik.videogamewishlist.syncadapter.SyncUtils
 import io.fabric.sdk.android.Fabric
 import kotlinx.android.synthetic.main.activity_welcome.*
 import org.jetbrains.anko.startActivity
-
+import java.net.URL
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var sharedPreferenceChangeListener: SharedPreferences.OnSharedPreferenceChangeListener
+
+    private lateinit var consentForm: ConsentForm
 
     private val isCacheFullySynced: Boolean
         @Synchronized get() = PreferenceManager.getDefaultSharedPreferences(applicationContext)
@@ -49,6 +52,10 @@ class WelcomeActivity : AppCompatActivity() {
                         or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
         SyncUtils.createSyncAccount(this)
 
+        checkConsentStatus()
+    }
+
+    private fun handleSync() {
         if (isCacheFullySynced) {
             startActivity(Intent(this@WelcomeActivity, WishlistActivity::class.java))
         } else {
@@ -64,5 +71,58 @@ class WelcomeActivity : AppCompatActivity() {
                     .registerOnSharedPreferenceChangeListener(
                             sharedPreferenceChangeListener)
         }
+    }
+
+    private fun checkConsentStatus() {
+        val consentInformation = ConsentInformation.getInstance(this)
+        val publisherIds = arrayOf("pub-8540885734073963")
+        consentInformation.requestConsentInfoUpdate(publisherIds, object : ConsentInfoUpdateListener {
+            override fun onConsentInfoUpdated(consentStatus: ConsentStatus) {
+                when(consentStatus) {
+                    ConsentStatus.PERSONALIZED -> handleSync()
+                    ConsentStatus.UNKNOWN, ConsentStatus.NON_PERSONALIZED -> {
+                        if (ConsentInformation.getInstance(this@WelcomeActivity)
+                                        .isRequestLocationInEeaOrUnknown) {
+                            requestConsent()
+                        } else {
+                            handleSync()
+                        }
+                    }
+                }
+            }
+
+            override fun onFailedToUpdateConsentInfo(errorDescription: String) {
+                // User's consent status failed to update.
+            }
+        })
+    }
+
+    private fun requestConsent() {
+        val privacyUrl = URL("https://esscheffer.github.io/VideoGameWishlist/")
+
+        consentForm = ConsentForm.Builder(this, privacyUrl)
+                .withListener(object : ConsentFormListener() {
+                    override fun onConsentFormLoaded() {
+                        consentForm.show()
+                    }
+
+                    override fun onConsentFormOpened() {
+                    }
+
+                    override fun onConsentFormClosed(
+                            consentStatus: ConsentStatus?, userPrefersAdFree: Boolean?) {
+                        when(consentStatus) {
+                            ConsentStatus.PERSONALIZED -> handleSync()
+                            else -> finish()
+                        }
+                    }
+
+                    override fun onConsentFormError(errorDescription: String?) {
+                    }
+                })
+                .withPersonalizedAdsOption()
+                .build()
+
+        consentForm.load()
     }
 }
